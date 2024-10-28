2f51f539f97dea1a8355ba0c898a903e218beac1
Memo <1805662380@qq.com>
2024-10-28 21:40:54 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker." + "memo3.site"]: dockerHub,
+  ["quay." + "memo3.site"]: "https://quay.io",
+  ["gcr." + "memo3.site"]: "https://gcr.io",
+  ["k8s-gcr." + "memo3.site"]: "https://k8s.gcr.io",
+  ["k8s." + "memo3.site"]: "https://registry.k8s.io",
+  ["ghcr." + "memo3.site"]: "https://ghcr.io",
+  ["cloudsmith." + "memo3.site"]: "https://docker.cloudsmith.io",
+  ["ecr." + "memo3.site"]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging." + "memo3.site"]: dockerHub,
 };
 
 function routeByHosts(host) {
